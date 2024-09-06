5a4e724ebce4315c171375c2f025c588c504d567
root <root@gramadevata-fe.yrlfdikjgn2utbm2vlxrrba0df.rx.internal.cloudapp.net>
2024-09-06 16:38:13 +0000

    Your commit message

--- gramadevatafe/src/app/app.config.ts
+++ gramadevatafe/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
