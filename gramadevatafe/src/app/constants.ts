5b3674f770347e2f9882397240986aaa3bbb1e63
venkymalla777 <venkymalla777@gmail.com>
2024-10-09 18:29:24 +0530

    adds

--- gramadevatafe/src/app/constants.ts
+++ gramadevatafe/src/app/constants.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'https://4.240.112.170/'
-export const BASE_URL = 'https://4.240.112.170/'
-// export const BASE_URL = 'http://127.0.0.1:8000/'
+// export const BASE_URL = 'https://4.240.112.170/'
+export const BASE_URL = 'http://127.0.0.1:8000/'
 export const URL = BASE_URL+"hindu/"
 export const INDIA = 'India'
